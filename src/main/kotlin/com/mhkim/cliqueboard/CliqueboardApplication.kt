fd26e85d5cc85f6c3402d781bf6087d5ed85e66a
kim <joakimtrinidad234@gmail.com>
2025-12-29 14:48:55 +0800

    first commit

---
+++ src/main/kotlin/com/mhkim/cliqueboard/CliqueboardApplication.kt
@@ -0,0 +1,11 @@
+package com.mhkim.cliqueboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CliqueboardApplication
+
+fun main(args: Array<String>) {
+	runApplication<CliqueboardApplication>(*args)
+}
---
+++ src/test/kotlin/com/mhkim/cliqueboard/CliqueboardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mhkim.cliqueboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CliqueboardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
